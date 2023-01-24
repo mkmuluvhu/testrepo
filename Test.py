206084ba558775979f0d3eb38ab5941cb7db43c7
mkmuluvhu <60699080+mkmuluvhu@users.noreply.github.com>
2023-01-24 03:16:17 +0200

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Mukondeleli Muluvhu** 
+# 
+# Business Inteligence Develper
+
+# *I am interested in data science because it feeds my curiousity and it is an evolving field*
+
+# ### My code is a function that is supposed take in the name of the guest as input and return a welcome massage
+
+# In[6]:
+
+
+def welcome(name):
+    print(f"Hi, {name}! Welcome to my assignment.")
+    
+welcome('Peer')
+
+
+# - 2023\. is a new year!
+# - Have a productive year.
+# 
+# Follow the instruction to find the answer to your everything
+# 
+# 1. Click on the link ("https://www.google.com>google</a>")
+
+# In[ ]:
+
+
+
+
